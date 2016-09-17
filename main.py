5bcedc7e93110da46c7ece4c8b410817124f4c6c
Michael Aquilina <michaelaquilina@gmail.com>
2016-09-17 23:36:46 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+import botocore
+import boto3
+
+
+class SyncClient(object):
+    def __init__(self, client, bucket, prefix):
+        self.client = client
+        self.bucket = bucket
+        self.prefix = prefix
+        self.get_sync_index()
+
+    def get_sync_index(self):
+        try:
+            data = self.client.get_object(
+                Bucket=self.bucket,
+                Key=os.path.join(self.prefix, '.syncindex'),
+            )['Body']
+            self.sync_index = json.loads(data.read().decode('utf-8'))
+        except botocore.exceptions.ClientError:
+            self.sync_index = {}
+
+    def put_sync_index(self):
+        self.client.put_object(
+            Bucket=self.bucket,
+            Key=os.path.join(self.prefix, '.syncindex'),
+            Body=json.dumps(self.sync_index),
+        )
+
+    def put_object(self, key, data, time_modified):
+        self.sync_index[key] = time_modified
+        self.client.put_object(
+            Bucket=self.bucket,
+            Key=os.path.join(self.prefix, key),
+            Body=data,
+        )
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--local')
+    parser.add_argument('--bucket')
+    parser.add_argument('--prefix')
+
+    args = parser.parse_args()
+
+    client = boto3.client('s3')
+    sync_client = SyncClient(client, args.bucket, args.prefix)
+
+    sync(sync_client, args.local)
+
+
+def sync(sync_client, local_dir):
+    local_index = get_local_index(local_dir)
+    perform_sync(sync_client, local_dir, local_index)
+
+
+def get_local_index(local_dir):
+    result = {}
+    for filename in os.listdir(local_dir):
+        absolute_path = os.path.join(local_dir, filename)
+        filestat = os.stat(absolute_path)
+        result[filename] = filestat.st_mtime
+    return result
+
+
+def perform_sync(sync_client, local_dir, local_index):
+    for key in local_index:
+        s3_object = sync_client.sync_index.get(key)
+        local_object = local_index[key]
+        local_path = os.path.join(local_dir, key)
+
+        if s3_object is None:
+            print('Need to upload (CREATE)', key)
+            with open(local_path, 'rb') as fp:
+                sync_client.put_object(key, fp, local_object)
+        elif local_object > s3_object:
+            print('Need to upload (UPDATE)', key)
+            with open(local_path, 'rb') as fp:
+                sync_client.put_object(key, fp, local_object)
+        else:
+            print('No need to update ', key)
+
+    sync_client.put_sync_index()
+
+
+if __name__ == '__main__':
+    main()
