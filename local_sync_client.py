3c2e31d143821d71f8519498649fed4764d9c453
Michael Aquilina <michaelaquilina@gmail.com>
2016-09-20 16:52:44 +0100

    Move LocalSyncClient and S3SyncClient into their own files

---
+++ local_sync_client.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+class LocalSyncClient(object):
+    def __init__(self, local_dir):
+        self.local_dir = local_dir
+
+    def get_object_timestamp(self, key):
+        object_path = os.path.join(self.local_dir, key)
+        if os.path.exists(object_path):
+            return os.stat(object_path).st_mtime
+        else:
+            return None
+
+    def update_sync_index(self):
+        pass
+
+    def keys(self):
+        for item in os.listdir(self.local_dir):
+            if item.startswith('.'):
+                continue
+
+            if os.path.isfile(os.path.join(self.local_dir, item)):
+                yield item
+
+    def put_object(self, key, fp, timestamp):
+        object_path = os.path.join(self.local_dir, key)
+        object_stat = os.stat(object_path)
+        with open(object_path, 'wb') as fp2:
+            fp2.write(fp.read())
+        os.utime(object_path, (object_stat.st_atime, timestamp))
+
+    def get_object(self, key):
+        return open(os.path.join(self.local_dir, key), 'rb')
--- main.py
+++ main.py
@@ -1,128 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import gzip
 import json
+import logging
 import os
 
-import botocore
 import boto3
 
-import logging
+
+from local_sync_client import LocalSyncClient
+from s3_sync_client import S3SyncClient
 
 
 logger = logging.getLogger(__name__)
-
-
-class LocalSyncClient(object):
-    def __init__(self, local_dir):
-        self.local_dir = local_dir
-
-    def get_object_timestamp(self, key):
-        object_path = os.path.join(self.local_dir, key)
-        if os.path.exists(object_path):
-            return os.stat(object_path).st_mtime
-        else:
-            return None
-
-    def update_sync_index(self):
-        pass
-
-    def keys(self):
-        for item in os.listdir(self.local_dir):
-            if item.startswith('.'):
-                continue
-
-            if os.path.isfile(os.path.join(self.local_dir, item)):
-                yield item
-
-    def put_object(self, key, fp, timestamp):
-        object_path = os.path.join(self.local_dir, key)
-        object_stat = os.stat(object_path)
-        with open(object_path, 'wb') as fp2:
-            fp2.write(fp.read())
-        os.utime(object_path, (object_stat.st_atime, timestamp))
-
-    def get_object(self, key):
-        return open(os.path.join(self.local_dir, key), 'rb')
-
-
-class S3SyncClient(object):
-    SYNC_INDEX = '.syncindex.json.gz'
-
-    def __init__(self, client, bucket, prefix):
-        self.client = client
-        self.bucket = bucket
-        self.prefix = prefix
-        self._get_sync_index()
-
-    def _mend_out_of_date_index(self):
-        for key, value in self.sync_index.items():
-            if not isinstance(value, dict):
-                self.sync_index[key] = {'timestamp': value, 'DateModified': None}
-
-    def _get_sync_index(self):
-        logger.info('Getting sync index from %s', self.bucket)
-        try:
-            data = self.client.get_object(
-                Bucket=self.bucket,
-                Key=os.path.join(self.prefix, self.SYNC_INDEX),
-            )['Body']
-            json_data = gzip.decompress(data.read()).decode('utf-8')
-            self.sync_index = json.loads(json_data)
-        except botocore.exceptions.ClientError:
-            logger.warning("Sync Index not found. Creating empty index")
-            self.sync_index = {}
-        else:
-            self._dirty_keys = set()
-
-        # Hack for backwards compatability. TODO: remove
-        self._mend_out_of_date_index()
-
-    def get_object_timestamp(self, key):
-        metadata = self.sync_index.get(key)
-        if metadata:
-            return metadata['timestamp']
-
-    def set_object_timestamp(self, key, timestamp):
-        if key not in self.sync_index:
-            self.sync_index[key] = {'LastModified': None}
-        self.sync_index[key]['timestamp'] = timestamp
-        self._dirty_keys.add(key)
-
-    def keys(self):
-        return self.sync_index.keys()
-
-    def update_sync_index(self):
-        if len(self._dirty_keys) > 0:
-            data = self.client.list_objects(Bucket=self.bucket, Prefix=self.prefix)
-            for s3_object in data['Contents']:
-                key = s3_object['Key'].replace(self.prefix, '', 1).lstrip('/')
-                if key in self._dirty_keys and key not in (self.SYNC_INDEX, '.syncindex'):
-                    timestamp = s3_object['LastModified'].timestamp()
-                    self.sync_index[key]['LastModified'] = timestamp
-
-            sync_data = json.dumps(self.sync_index).encode('utf-8')
-
-            self.client.put_object(
-                Bucket=self.bucket,
-                Key=os.path.join(self.prefix, self.SYNC_INDEX),
-                Body=gzip.compress(sync_data),
-            )
-            self._dirty_keys.clear()
-
-    def put_object(self, key, fp, timestamp):
-        self.client.put_object(
-            Bucket=self.bucket,
-            Key=os.path.join(self.prefix, key),
-            Body=fp,
-        )
-        self.set_object_timestamp(key, timestamp)
-
-    def get_object(self, key):
-        return self.client.get_object(
-            Bucket=self.bucket,
-            Key=os.path.join(self.prefix, key),
-        )['Body']
 
 
 def sync():
