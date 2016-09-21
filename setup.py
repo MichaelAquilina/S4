ddeaab14922ff656b63984367c85d47bebfd4183
Michael Aquilina <michaelaquilina@gmail.com>
2016-09-21 17:57:55 +0100

    Add tox.ini and setup.cfg

---
+++ setup.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='s3backup',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a Python package',
+    author='Michael Aquilina',
+    license='BSD',
+    author_email='michaelaquilina@gmail.com',
+    url='https://github.com/audreyr/cookiecutter-pypackage',
+    keywords=['aws', 's3', 'backup'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
