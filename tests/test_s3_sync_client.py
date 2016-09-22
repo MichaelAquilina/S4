ebd5344261a62e988a3a5ec04c0834877dc26e83
Michael Aquilina <michaelaquilina@gmail.com>
2016-09-22 13:48:53 +0100

    Add test_get_object_timestamp

--- tests/test_s3_sync_client.py
+++ tests/test_s3_sync_client.py
@@ -9,6 +9,14 @@ import moto
 from s3backup.s3_sync_client import S3SyncClient
 
 
+def set_index(client, bucket, key, data):
+    client.put_object(
+        Bucket='testbucket',
+        Key='{}/.syncindex.json.gz'.format(key),
+        Body=gzip.compress(json.dumps(data).encode('utf-8'))
+    )
+
+
 class TestS3SyncClient(object):
 
     @moto.mock_s3
@@ -28,12 +36,23 @@ class TestS3SyncClient(object):
             'foo': {'timestamp': 123213213, 'DateModified': 423232},
             'bar': {'timestamp': 231412323, 'DateModified': 324232},
         }
-
-        client.put_object(
-            Bucket='testbucket',
-            Key='Music/.syncindex.json.gz',
-            Body=gzip.compress(json.dumps(sync_index).encode('utf-8'))
-        )
+        set_index(client, 'testbucket', 'Music', sync_index)
 
         sync_client = S3SyncClient(client, 'testbucket', 'Music/')
         assert sync_client.sync_index == sync_index
+
+    @moto.mock_s3
+    def test_get_object_timestamp(self):
+        client = boto3.client('s3')
+        client.create_bucket(Bucket='testbucket')
+
+        sync_index = {
+            'foo': {'timestamp': 123213213, 'DateModified': 423232},
+            'bar': {'timestamp': 231412323, 'DateModified': 324232},
+        }
+        set_index(client, 'testbucket', 'Music', sync_index)
+
+        sync_client = S3SyncClient(client, 'testbucket', 'Music/')
+        assert sync_client.get_object_timestamp('foo') == 123213213
+        assert sync_client.get_object_timestamp('bar') == 231412323
+        assert sync_client.get_object_timestamp('idontexist') is None
