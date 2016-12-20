4f6bbc41da084da7cfa407da7147468b5ca81a6e
Michael Aquilina <michaelaquilina@gmail.com>
2016-12-20 22:49:01 +0100

    Refactor function names

--- s3backup/main.py
+++ s3backup/main.py
@@ -5,7 +5,7 @@ from s3backup.clients import compare, LocalSyncClient
 
 def sync():
     local_client = LocalSyncClient('/home/michael/Notebooks')
-    # print(local_client.index())
-    # print(local_client.current())
-    print(list(compare(local_client.index(), local_client.current())))
-    local_client.save_index()
+    current = local_client.get_current_state()
+    index = local_client.get_index_state()
+    print(list(compare(current, index)))
+    local_client.update_index()
