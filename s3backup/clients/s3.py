ce16e1ebaa55747369ab7a1cf486054e3241c2d6
Michael Aquilina <michaelaquilina@gmail.com>
2016-12-21 18:16:32 +0100

    Add s3 sync client and tests

---
+++ s3backup/clients/s3.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import os
+
+from botocore.exceptions import ClientError
+
+from s3backup.sync import FileEntry
+
+
+def to_timestamp(dt):
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+    return (dt - epoch) / datetime.timedelta(seconds=1)
+
+
+class S3SyncClient(object):
+
+    def __init__(self, client, bucket, prefix):
+        self.client = client
+        self.bucket = bucket
+        self.prefix = prefix
+
+    def index_path(self):
+        return os.path.join(self.prefix, '.index')
+
+    def get_absolute_path(self, path):
+        return os.path.join(self.prefix, path)
+
+    def get_index_state(self):
+        try:
+            resp = self.client.get_object(
+                Bucket=self.bucket,
+                Key=self.index_path(),
+            )
+        except ClientError:
+            return {}
+        else:
+            data = json.loads(resp['Body'].read().decode('utf-8'))
+            results = {}
+            for path, metadata in data.items():
+                results[path] = FileEntry(path, timestamp=metadata['timestamp'])
+
+            return results
+
+    def get_current_state(self):
+        results = {}
+        resp = self.client.list_objects_v2(
+            Bucket=self.bucket,
+            Prefix=self.prefix,
+        )
+        for obj in resp['Contents']:
+            key = os.path.relpath(obj['Key'], self.prefix)
+            if key == '.index':
+                continue
+
+            results[key] = FileEntry(
+                path=key,
+                timestamp=to_timestamp(obj['LastModified']),
+            )
+        return results
+
+    def update_index(self):
+        data = {}
+        for key, entry in self.get_current_state().items():
+            data[key] = {'timestamp': entry.timestamp}
+
+        self.client.put_object(
+            Bucket=self.bucket,
+            Key=self.index_path(),
+            Body=json.dumps(data),
+        )
--- s3backup/main.py
+++ s3backup/main.py
@@ -2,14 +2,28 @@
 
 import os
 
+import boto3
+
 from s3backup.clients.local import LocalSyncClient
+from s3backup.clients.s3 import S3SyncClient
 from s3backup.sync import compare_states
 
 
 def sync():
     target_folder = os.path.expanduser('~/Notebooks')
 
+    bucket = 'michaelaquilina.data2'
+    prefix = 'ZimNoteBooks'
+    client = boto3.client('s3')
+
     local_client = LocalSyncClient(target_folder)
+    s3_client = S3SyncClient(client, bucket, prefix)
+
+    current = s3_client.get_current_state()
+    index = s3_client.get_index_state()
+    print(list(compare_states(current, index)))
+    s3_client.update_index()
+
     current = local_client.get_current_state()
     index = local_client.get_index_state()
     print(list(compare_states(current, index)))
