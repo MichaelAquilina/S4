c3e63c69f36ee60d8fd9ab14983e09416ae05db3
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-04 11:46:55 +0000

    Move generic objects out of LocalSyncClient file

--- s3backup/clients/local.py
+++ s3backup/clients/local.py
@@ -4,6 +4,8 @@ import datetime
 import json
 import os
 
+from s3backup.clients import SyncAction, SyncObject
+
 
 def traverse(path, ignore_files=None):
     if ignore_files is None:
@@ -16,38 +18,6 @@ def traverse(path, ignore_files=None):
                 yield os.path.join(item, result)
         elif item not in ignore_files:
             yield item
-
-
-class SyncAction(object):
-    UPDATE = 'UPDATE'
-    DELETE = 'DELETE'
-    CONFLICT = 'CONFLICT'
-    NONE = 'NONE'
-
-    def __init__(self, action, timestamp):
-        self.action = action
-        self.timestamp = timestamp
-
-    def __eq__(self, other):
-        if not isinstance(other, SyncAction):
-            return False
-        return self.action == other.action and self.timestamp == other.timestamp
-
-    def __repr__(self):
-        if self.timestamp is not None:
-            timestamp = datetime.datetime.utcfromtimestamp(self.timestamp)
-        else:
-            timestamp = None
-        return 'SyncAction<{}, {}>'.format(self.action, timestamp)
-
-
-class SyncObject(object):
-    def __init__(self, fp, timestamp):
-        self.fp = fp
-        self.timestamp = timestamp
-
-    def __repr__(self):
-        return 'SyncObject<{}, {}>'.format(self.fp, self.timestamp)
 
 
 class LocalSyncClient(object):
