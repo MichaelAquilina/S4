758ef5c4a16a245a70f48bd3d91c62bc6aee4c54
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-04 21:29:07 +0000

    More tests and fixes for S3SyncClient

--- tests/clients/test_s3.py
+++ tests/clients/test_s3.py
@@ -13,7 +13,7 @@ from moto import mock_s3
 
 import pytest
 
-from s3backup.clients import s3, SyncObject
+from s3backup.clients import s3, SyncAction, SyncObject
 
 
 def touch(client, bucket, key, timestamp=None):
@@ -37,6 +37,18 @@ class TestS3SyncClient(object):
 
         client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
         assert repr(client) == 'S3SyncClient<testbucket, foo/bar>'
+
+    @mock_s3
+    def test_index_path(self):
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            aws_session_token='',
+        )
+
+        client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
+        assert client.index_path() == 'foo/bar/.index'
 
     @mock_s3
     def test_put(self):
@@ -200,3 +212,150 @@ class TestS3SyncClient(object):
 
         assert client.get_remote_timestamp('world') == 5000
         assert client.get_index_local_timestamp('world') is None
+
+    @mock_s3
+    def test_update_index(self):
+        # given
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            aws_session_token='',
+        )
+        s3_client.create_bucket(Bucket='testbucket')
+        s3_client.put_object(Bucket='testbucket', Key='foo/bar/.index', Body=json.dumps({
+            'red': {
+                'remote_timestamp': 1234,
+                'local_timestamp': 1200,
+            },
+            'green': {
+                'remote_timestamp': 5000,
+            }
+        }))
+        touch(s3_client, 'testbucket', 'foo/bar/red', 5001)
+        touch(s3_client, 'testbucket', 'foo/bar/yellow', 1000)
+        touch(s3_client, 'testbucket', 'foo/bar/orange', 2000)
+
+        # when
+        client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
+        client.update_index()
+
+        # then
+        expected_index = {
+            'red': {
+                'remote_timestamp': 1234,
+                'local_timestamp': 5001,
+            },
+            'green': {
+                'remote_timestamp': 5000,
+                'local_timestamp': None,
+            },
+            'yellow': {
+                'remote_timestamp': None,
+                'local_timestamp': 1000,
+            },
+            'orange': {
+                'remote_timestamp': None,
+                'local_timestamp': 2000,
+            },
+        }
+        assert client.index == expected_index
+
+    @mock_s3
+    def test_get_real_local_timestamp(self):
+        # given
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            aws_session_token='',
+        )
+        s3_client.create_bucket(Bucket='testbucket')
+        touch(s3_client, 'testbucket', 'foo/bar/orange', 2000)
+
+        client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
+
+        # then
+        assert client.get_real_local_timestamp('orange') == 2000
+        assert client.get_real_local_timestamp('idontexist') is None
+
+    @mock_s3
+    def test_set_index_timestamps(self):
+        # given
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            aws_session_token='',
+        )
+        s3_client.create_bucket(Bucket='testbucket')
+        s3_client.put_object(Bucket='testbucket', Key='foo/bar/.index', Body=json.dumps({
+            'red': {
+                'remote_timestamp': 1234,
+                'local_timestamp': 1200,
+            },
+        }))
+
+        # when
+        client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
+        client.set_index_local_timestamp('red', 3000)
+        client.set_remote_timestamp('red', 4000)
+        client.set_index_local_timestamp('green', 5000)
+        client.set_remote_timestamp('yellow', 6000)
+
+        # then
+        expected_index = {
+            'red': {
+                'local_timestamp': 3000,
+                'remote_timestamp': 4000,
+            },
+            'green': {
+                'local_timestamp': 5000,
+            },
+            'yellow': {
+                'remote_timestamp': 6000,
+            },
+        }
+        assert client.index == expected_index
+
+    @mock_s3
+    def test_get_action(self):
+        # given
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            aws_session_token='',
+        )
+        s3_client.create_bucket(Bucket='testbucket')
+        s3_client.put_object(Bucket='testbucket', Key='foo/bar/.index', Body=json.dumps({
+            'foo': {
+                'local_timestamp': 4000,
+            },
+            'bar': {
+                'local_timestamp': 1000,
+                'remote_timestamp': 1100,
+            },
+            'baz': {
+                'local_timestamp': 1111,
+                'remote_timestamp': 1400,
+            },
+            'ooo': {
+                'local_timestamp': 9999,
+            },
+            'ppp': {
+                'local_timestamp': None,
+                'remote_timestamp': 4000,
+            }
+        }))
+        touch(s3_client, 'testbucket', 'foo/bar/foo', 5000)
+        touch(s3_client, 'testbucket', 'foo/bar/baz', 1111)
+        touch(s3_client, 'testbucket', 'foo/bar/ooo', 1000)
+
+        client = s3.S3SyncClient(s3_client, 'testbucket', 'foo/bar')
+        assert client.get_action('foo') == SyncAction(SyncAction.UPDATE, 5000)
+        assert client.get_action('bar') == SyncAction(SyncAction.DELETE, 1100)
+        assert client.get_action('baz') == SyncAction(SyncAction.NONE, 1400)
+        assert client.get_action('ooo') == SyncAction(SyncAction.CONFLICT, 9999)
+        assert client.get_action('ppp') == SyncAction(SyncAction.DELETE, 4000)
+        assert client.get_action('dontexist') == SyncAction(SyncAction.NONE, None)
