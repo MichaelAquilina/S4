fb22768ebd0fe2143da9404c264ddb63169d7a78
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-04 13:57:29 +0000

    Work in progress update to S3SyncClient but no tests...

--- s3backup/clients/s3.py
+++ s3backup/clients/s3.py
@@ -5,42 +5,41 @@ import os
 
 from botocore.exceptions import ClientError
 
+from s3backup.clients import SyncClient, SyncObject
+
 
 def to_timestamp(dt):
     epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
     return (dt - epoch) / datetime.timedelta(seconds=1)
 
 
-class S3SyncClient(object):
-
+class S3SyncClient(SyncClient):
     def __init__(self, client, bucket, prefix):
         self.client = client
         self.bucket = bucket
         self.prefix = prefix
-        self.index = self.get_index_state()
+        self.index = self.load_index()
+
+    def __repr__(self):
+        return 'S3SyncClient<{}, {}>'.format(self.bucket, self.prefix)
 
     def index_path(self):
         return os.path.join(self.prefix, '.index')
 
-    def get_absolute_path(self, path):
-        return os.path.join(self.prefix, path)
-
-    def put(self, key, fp, remote_timestamp):
+    def put(self, key, sync_object):
         self.client.put_object(
             Bucket=self.bucket,
             Key=os.path.join(self.prefix, key),
-            Body=fp,
+            Body=sync_object.fp,
         )
-        if key not in self.index:
-            self.index[key] = {}
-        self.index[key]['remote_timestamp'] = remote_timestamp
+        self.set_remote_timestamp(key, sync_object.timestamp)
 
     def get(self, key):
         resp = self.client.get_object(
             Bucket=self.bucket,
             Key=os.path.join(self.prefix, key),
         )
-        return resp['Body']
+        return SyncObject(resp['Body'], to_timestamp(resp['LastModified']))
 
     def delete(self, key):
         self.client.delete_object(
@@ -48,7 +47,7 @@ class S3SyncClient(object):
             Key=os.path.join(self.prefix, key),
         )
 
-    def get_index_state(self):
+    def load_index(self):
         try:
             resp = self.client.get_object(
                 Bucket=self.bucket,
@@ -59,8 +58,24 @@ class S3SyncClient(object):
         except (ClientError, ValueError):
             return {}
 
-    def get_current_state(self):
-        results = {}
+    def update_index(self):
+        keys = self.get_all_keys()
+        index = {}
+        for key in keys:
+            index[key] = {
+                'remote_timestamp': self.get_remote_timestamp(key),
+                'local_timestamp': self.get_real_local_timestamp(key),
+            }
+
+        self.client.put_object(
+            Bucket=self.bucket,
+            Key=self.index_path(),
+            Body=json.dumps(index),
+        )
+        self.index = index
+
+    def get_local_keys(self):
+        results = []
         resp = self.client.list_objects_v2(
             Bucket=self.bucket,
             Prefix=self.prefix,
@@ -72,20 +87,32 @@ class S3SyncClient(object):
             key = os.path.relpath(obj['Key'], self.prefix)
             if key == '.index':
                 continue
+            results.append(key)
 
-            results[key] = {
-                'local_timestamp': to_timestamp(obj['LastModified']),
-            }
         return results
 
-    def update_index(self):
-        results = self.get_current_state()
-        for key in results:
-            if key in self.index:
-                results[key]['remote_timestamp'] = self.index[key]['remote_timestamp']
-
-        self.client.put_object(
+    def get_real_local_timestamp(self, key):
+        response = self.client.head_object(
             Bucket=self.bucket,
-            Key=self.index_path(),
-            Body=json.dumps(results),
+            Key=os.path.join(self.prefix, key),
         )
+        return to_timestamp(response['LastModified'])
+
+    def get_index_keys(self):
+        return self.index.keys()
+
+    def get_index_local_timestamp(self, key):
+        return self.index.get(key, {}).get('local_timestamp')
+
+    def set_index_local_timestamp(self, key, timestamp):
+        if key not in self.index:
+            self.index[key] = {}
+        self.index[key]['local_timestamp'] = timestamp
+
+    def get_remote_timestamp(self, key):
+        return self.index.get(key, {}).get('remote_timestamp')
+
+    def set_remote_timestamp(self, key, timestamp):
+        if key not in self.index:
+            self.index[key] = {}
+        self.index[key]['remote_timestamp'] = timestamp
