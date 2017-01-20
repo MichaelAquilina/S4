08d196fff68933719b0826a987e189f49adefa23
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-20 14:38:06 +0000

    Slightly improved conflict message

--- s3backup/sync.py
+++ s3backup/sync.py
@@ -42,9 +42,9 @@ def sync(client_1, client_2):
         for key, (action_1, action_2) in unhandled_events.items():
             logger.info(
                 'Conflict for %s\n'
-                'which version would you like to keep\n'
-                '   %s at %s on %s (1) or\n'
-                '   %s at %s on %s (2)?',
+                'which version would you like to keep?\n'
+                '   %s at %s on %s (1)\n'
+                '   %s at %s on %s (2)',
                 key,
                 action_1.action, action_1.get_datetime(), client_1.get_uri(),
                 action_2.action, action_2.get_datetime(), client_2.get_uri(),
