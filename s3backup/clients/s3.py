ebafdd415bedc6f2c7b4416133f384d036487522
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-31 16:16:56 +0000

    Refactor test_s3.py to use s3_client fixture

--- s3backup/clients/s3.py
+++ s3backup/clients/s3.py
@@ -65,11 +65,18 @@ class S3SyncClient(SyncClient):
         self.set_remote_timestamp(key, sync_object.timestamp)
 
     def get(self, key):
-        resp = self.client.get_object(
-            Bucket=self.bucket,
-            Key=os.path.join(self.prefix, key),
-        )
-        return SyncObject(resp['Body'], resp['ContentLength'], to_timestamp(resp['LastModified']))
+        try:
+            resp = self.client.get_object(
+                Bucket=self.bucket,
+                Key=os.path.join(self.prefix, key),
+            )
+            return SyncObject(
+                resp['Body'],
+                resp['ContentLength'],
+                to_timestamp(resp['LastModified']),
+            )
+        except ClientError:
+            return None
 
     def delete(self, key):
         resp = self.client.delete_objects(
