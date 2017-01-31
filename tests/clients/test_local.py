66dadadb5a03db872cf603091518cedd41a1031f
Michael Aquilina <michaelaquilina@gmail.com>
2017-01-31 22:44:18 +0000

    Use fixtures in test_local.py

--- tests/clients/test_local.py
+++ tests/clients/test_local.py
@@ -54,89 +54,73 @@ class TestTraverse(object):
         assert sorted(actual_output) == sorted(expected_output)
 
 
+def get_file_data(client, key):
+    with open(os.path.join(client.path, key), 'rb') as fp:
+        return fp.read()
+
+def set_file_data(client, key, data):
+    with open(os.path.join(client.path, key), 'wb') as fp:
+        fp.write(data)
+
+def set_index(client, data):
+    with open(client.index_path(), 'w') as fp:
+        json.dump(data, fp)
+    client.reload_index()
+
+
 class TestLocalSyncClient(object):
-    def setup_method(self):
-        self.target_folder = tempfile.mkdtemp()
-        self.index_path = os.path.join(self.target_folder, '.index')
-
-    def teardown_method(self):
-        shutil.rmtree(self.target_folder)
-
-    def test_repr(self):
-        client = local.LocalSyncClient('/home/rick/timemachine/')
-        assert repr(client) == 'LocalSyncClient</home/rick/timemachine/>'
-
-    def set_index(self, data):
-        with open(self.index_path, 'w') as fp:
-            json.dump(data, fp)
-
-    def get_file_data(self, key):
-        with open(os.path.join(self.target_folder, key), 'rb') as fp:
-            return fp.read()
-
-    def set_file_data(self, key, data):
-        with open(os.path.join(self.target_folder, key), 'wb') as fp:
-            fp.write(data)
-
-    def test_put_new(self):
-        client = local.LocalSyncClient(self.target_folder)
+    def test_put_new(self, local_client):
         data = b'hi'
-        client.put(
+        local_client.put(
             key='hello_world.txt',
             sync_object=SyncObject(io.BytesIO(data), len(data), 20000)
         )
 
-        assert client.index['hello_world.txt']['remote_timestamp'] == 20000
-        assert self.get_file_data('hello_world.txt') == data
+        assert local_client.index['hello_world.txt']['remote_timestamp'] == 20000
+        assert get_file_data(local_client, 'hello_world.txt') == data
 
-    def test_put_existing(self):
-        self.set_index({
+    def test_put_existing(self, local_client):
+        set_index(local_client, {
             'doge.txt': {'local_timestamp': 1111111}
         })
 
         data = b'canis lupus familiaris'
-        client = local.LocalSyncClient(self.target_folder)
-        client.put(
+        local_client.put(
             key='doge.txt',
             sync_object=SyncObject(io.BytesIO(data), len(data), 20000)
         )
 
-        assert client.index['doge.txt']['remote_timestamp'] == 20000
-        assert client.index['doge.txt']['local_timestamp'] == 1111111
+        assert local_client.index['doge.txt']['remote_timestamp'] == 20000
+        assert local_client.index['doge.txt']['local_timestamp'] == 1111111
 
-        assert self.get_file_data('doge.txt') == data
+        assert get_file_data(local_client, 'doge.txt') == data
 
-    def test_get_existing(self):
-        client = local.LocalSyncClient(self.target_folder)
-
+    def test_get_existing(self, local_client):
         data = b'blue green yellow'
-        self.set_file_data('whatup.md', data)
-        sync_object = client.get('whatup.md')
+        set_file_data(local_client, 'whatup.md', data)
+        sync_object = local_client.get('whatup.md')
         assert sync_object.fp.read() == data
         assert sync_object.total_size == len(data)
 
-    def test_get_non_existant(self):
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.get('idontexist.md') is None
+    def test_get_non_existant(self, local_client):
+        assert local_client.get('idontexist.md') is None
 
-    def test_delete_existing(self):
-        target_file = os.path.join(self.target_folder, 'foo')
-        touch(os.path.join(self.target_folder, 'foo'), 222222)
-        client = local.LocalSyncClient(self.target_folder)
+    def test_delete_existing(self, local_client):
+        target_file = os.path.join(local_client.path, 'foo')
+        touch(os.path.join(local_client.path, 'foo'), 222222)
 
         assert os.path.exists(target_file) is True
-        assert client.delete('foo') is True
+        assert local_client.delete('foo') is True
         assert os.path.exists(target_file) is False
 
-    def test_delete_non_existant(self):
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.delete('idontexist.txt') is False
+    def test_delete_non_existant(self, local_client):
+        assert local_client.delete('idontexist.txt') is False
 
     def test_index_path(self):
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.index_path() == self.index_path
+        client = local.LocalSyncClient('/hello/from/the/magic/tavern')
+        assert client.index_path() == '/hello/from/the/magic/tavern/.index'
 
-    def test_load_index(self):
+    def test_load_index(self, local_client):
         data = {
             'foo': {
                 'local_timestamp': 4000,
@@ -147,23 +131,21 @@ class TestLocalSyncClient(object):
                 'remote_timestamp': 5000,
             },
         }
-        self.set_index(data)
+        set_index(local_client, data)
 
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.index == data
+        assert local_client.index == data
 
-    def test_get_local_keys(self):
-        self.set_index({})  # .index file should not come up in results
-        touch(os.path.join(self.target_folder, '.bashrc'))
-        touch(os.path.join(self.target_folder, 'foo'))
-        touch(os.path.join(self.target_folder, 'bar'))
+    def test_get_local_keys(self, local_client):
+        set_index(local_client, {})  # .index file should not come up in results
+        touch(os.path.join(local_client.path, '.bashrc'))
+        touch(os.path.join(local_client.path, 'foo'))
+        touch(os.path.join(local_client.path, 'bar'))
 
-        client = local.LocalSyncClient(self.target_folder)
-        actual_output = client.get_local_keys()
+        actual_output = local_client.get_local_keys()
         expected_output = ['foo', 'bar', '.bashrc']
         assert sorted(expected_output) == sorted(actual_output)
 
-    def test_get_index_keys(self):
+    def test_get_index_keys(self, local_client):
         data = {
             'foo': {
                 'local_timestamp': 4000,
@@ -174,16 +156,16 @@ class TestLocalSyncClient(object):
                 'remote_timestamp': 5000,
             },
         }
-        self.set_index(data)
-        client = local.LocalSyncClient(self.target_folder)
-        actual_output = client.get_index_keys()
+        set_index(local_client, data)
+
+        actual_output = local_client.get_index_keys()
         expected_output = ['foo', 'bar/baz.txt']
         assert sorted(list(actual_output)) == sorted(expected_output)
 
-    def test_all_keys(self):
-        touch(os.path.join(self.target_folder, '.index'))
-        touch(os.path.join(self.target_folder, 'foo'))
-        touch(os.path.join(self.target_folder, 'bar/boo.md'))
+    def test_all_keys(self, local_client):
+        touch(os.path.join(local_client.path, '.index'))
+        touch(os.path.join(local_client.path, 'foo'))
+        touch(os.path.join(local_client.path, 'bar/boo.md'))
         data = {
             'foo': {
                 'local_timestamp': 4000,
@@ -194,18 +176,17 @@ class TestLocalSyncClient(object):
                 'remote_timestamp': 5000,
             },
         }
-        self.set_index(data)
-        client = local.LocalSyncClient(self.target_folder)
-        actual_output = client.get_all_keys()
+        set_index(local_client, data)
+
+        actual_output = local_client.get_all_keys()
         expected_output = ['foo', 'bar/boo.md', 'bar/baz.txt']
         assert sorted(actual_output) == sorted(expected_output)
 
-    def test_update_index_empty(self):
-        touch(os.path.join(self.target_folder, 'foo'), 13371337)
-        touch(os.path.join(self.target_folder, 'bar'), 50032003)
+    def test_update_index_empty(self, local_client):
+        touch(os.path.join(local_client.path, 'foo'), 13371337)
+        touch(os.path.join(local_client.path, 'bar'), 50032003)
 
-        client = local.LocalSyncClient(self.target_folder)
-        client.update_index()
+        local_client.update_index()
         # remote timestamp should not be included since it does not exist
         expected_output = {
             'foo': {
@@ -217,13 +198,13 @@ class TestLocalSyncClient(object):
                 'remote_timestamp': None,
             },
         }
-        assert client.index == expected_output
+        assert local_client.index == expected_output
 
-    def test_update_index_non_empty(self):
-        touch(os.path.join(self.target_folder, 'foo'), 13371337)
-        touch(os.path.join(self.target_folder, 'bar'), 50032003)
+    def test_update_index_non_empty(self, local_client):
+        touch(os.path.join(local_client.path, 'foo'), 13371337)
+        touch(os.path.join(local_client.path, 'bar'), 50032003)
 
-        self.set_index({
+        set_index(local_client, {
             'foo': {
                 'local_timestamp': 4000,
                 'remote_timestamp': 4000,
@@ -238,8 +219,7 @@ class TestLocalSyncClient(object):
             }
         })
 
-        client = local.LocalSyncClient(self.target_folder)
-        client.update_index()
+        local_client.update_index()
         expected_output = {
             'foo': {
                 'local_timestamp': 13371337,
@@ -254,29 +234,27 @@ class TestLocalSyncClient(object):
                 'remote_timestamp': 5000,
             },
         }
-        assert client.index == expected_output
+        assert local_client.index == expected_output
 
-    def test_get_real_local_timestamp(self):
-        touch(os.path.join(self.target_folder, 'atcg'), 2323230)
+    def test_get_real_local_timestamp(self, local_client):
+        touch(os.path.join(local_client.path, 'atcg'), 2323230)
 
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.get_real_local_timestamp('atcg') == 2323230
-        assert client.get_real_local_timestamp('dontexist') is None
+        assert local_client.get_real_local_timestamp('atcg') == 2323230
+        assert local_client.get_real_local_timestamp('dontexist') is None
 
-    def test_get_all_real_local_timestamps(self):
-        touch(os.path.join(self.target_folder, 'red'), 2323230)
-        touch(os.path.join(self.target_folder, 'blue'), 80808008)
+    def test_get_all_real_local_timestamps(self, local_client):
+        touch(os.path.join(local_client.path, 'red'), 2323230)
+        touch(os.path.join(local_client.path, 'blue'), 80808008)
 
-        client = local.LocalSyncClient(self.target_folder)
         expected_output = {
             'red': 2323230,
             'blue': 80808008,
         }
-        actual_output = client.get_all_real_local_timestamps()
+        actual_output = local_client.get_all_real_local_timestamps()
         assert actual_output == expected_output
 
-    def test_get_index_local_timestamp(self):
-        self.set_index({
+    def test_get_index_local_timestamp(self, local_client):
+        set_index(local_client, {
             'foo': {
                 'local_timestamp': 4000,
                 'remote_timestamp': 32000,
@@ -285,17 +263,16 @@ class TestLocalSyncClient(object):
                 'local_timestamp': 9999999,
             }
         })
-        client = local.LocalSyncClient(self.target_folder)
-        assert client.get_index_local_timestamp('foo') == 4000
-        assert client.get_index_local_timestamp('dontexist') is None
-        assert client.get_index_local_timestamp('bar') == 9999999
+        assert local_client.get_index_local_timestamp('foo') == 4000
+        assert local_client.get_index_local_timestamp('dontexist') is None
+        assert local_client.get_index_local_timestamp('bar') == 9999999
 
-        assert client.get_remote_timestamp('foo') == 32000
-        assert client.get_remote_timestamp('dontexist') is None
-        assert client.get_remote_timestamp('bar') is None
+        assert local_client.get_remote_timestamp('foo') == 32000
+        assert local_client.get_remote_timestamp('dontexist') is None
+        assert local_client.get_remote_timestamp('bar') is None
 
-    def test_get_all_index_local_timestamps(self):
-        self.set_index({
+    def test_get_all_index_local_timestamps(self, local_client):
+        set_index(local_client, {
             'frap': {
                 'local_timestamp': 4000,
             },
@@ -304,16 +281,15 @@ class TestLocalSyncClient(object):
             }
         })
 
-        client = local.LocalSyncClient(self.target_folder)
         expected_output = {
             'frap': 4000,
             'brap': 9999999,
         }
-        actual_output = client.get_all_index_local_timestamps()
+        actual_output = local_client.get_all_index_local_timestamps()
         assert actual_output == expected_output
 
-    def test_get_all_remote_timestamps(self):
-        self.set_index({
+    def test_get_all_remote_timestamps(self, local_client):
+        set_index(local_client, {
             'frap': {
                 'remote_timestamp': 4000,
             },
@@ -322,10 +298,9 @@ class TestLocalSyncClient(object):
             }
         })
 
-        client = local.LocalSyncClient(self.target_folder)
         expected_output = {
             'frap': 4000,
             'brap': 9999999,
         }
-        actual_output = client.get_all_remote_timestamps()
+        actual_output = local_client.get_all_remote_timestamps()
         assert actual_output == expected_output
