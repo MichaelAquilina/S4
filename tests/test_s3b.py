9406021abb86e8b1b17598b9daba874ad718ffb8
Michael Aquilina <michaelaquilina@gmail.com>
2017-03-04 13:13:53 +0000

    Add tests for ls command

--- tests/test_s3b.py
+++ tests/test_s3b.py
@@ -1,24 +1,61 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import io
 import logging
+
+import pytest
 
 import s3b
 
 
+@pytest.fixture
+def logger():
+    stream = io.StringIO()
+    result = logging.getLogger('test_targets')
+    result.setLevel(logging.INFO)
+    result.handlers = []
+    result.addHandler(logging.StreamHandler(stream))
+    return result
+
+
+def get_stream_value(logger):
+    return logger.handlers[0].stream.getvalue()
+
+
+class TestLsCommand(object):
+    def test_empty_config(self, logger):
+        args = argparse.Namespace(target='idontexist')
+        s3b.ls_command(args, {}, logger)
+        expected_result = (
+            'You have not added any targets yet\n'
+            'Use the "add" command to do this\n'
+        )
+        assert get_stream_value(logger) == expected_result
+
+    def test_missing_target(self, logger):
+        args = argparse.Namespace(target='idontexist')
+        config = {
+            'targets': {
+                'foo': {},
+                'bar': {},
+            }
+        }
+        s3b.ls_command(args, config, logger)
+        expected_result = (
+            '"idontexist" is an unknown target\n'
+            'Choices are: [\'bar\', \'foo\']\n'
+        )
+        assert get_stream_value(logger) == expected_result
+
+
 class TestTargetsCommand(object):
 
-    def setup_method(self):
-        self.fp = io.StringIO()
-        self.logger = logging.getLogger('test_targets')
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler(self.fp))
+    def test_empty(self, logger):
+        s3b.targets_command(None, {}, logger)
+        assert get_stream_value(logger) == ''
 
-    def test_empty(self):
-        s3b.targets_command(None, {}, self.logger)
-        assert self.fp.getvalue() == ''
-
-    def test_correct_output(self):
+    def test_correct_output(self, logger):
         config = {
             'targets': {
                 'Personal': {
@@ -32,10 +69,10 @@ class TestTargetsCommand(object):
             }
         }
 
-        s3b.targets_command(None, config, self.logger)
+        s3b.targets_command(None, config, logger)
 
         expected_result = (
             'Personal: [/home/user/Documents <=> s3://mybackup/Personal]\n'
             'Studies: [/media/backup/Studies <=> s3://something/something/Studies]\n'
         )
-        assert self.fp.getvalue() == expected_result
+        assert get_stream_value(logger) == expected_result
