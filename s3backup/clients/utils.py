2b7f91f0e903330081cc0bc93d295eac382e542d
Michael Aquilina <michaelaquilina@gmail.com>
2017-03-05 01:12:30 +0000

    test correct output for ls command

---
+++ s3backup/clients/utils.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+def to_timestamp(dt):
+    epoch = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
+    return (dt - epoch) / datetime.timedelta(seconds=1)
