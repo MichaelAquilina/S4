25b9d5b107206748dea7515a2e2fbff1edec8a08
Michael Aquilina <michaelaquilina@gmail.com>
2017-07-20 22:34:26 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='s4',
     packages=[],
-    version='0.1.0',
+    version='0.1.1',
     description='Sync your folders to s3 between multiple machines',
     author='Michael Aquilina',
     license='MIT',
