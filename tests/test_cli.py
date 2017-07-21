ea7c99ff65aea48907c1d6fc4d5c3dccd6d71bcb
Michael Aquilina <michaelaquilina@gmail.com>
2017-07-21 23:50:07 +0100

    Add test for no changes on edit command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -82,11 +82,48 @@ class TestEditCommand(object):
         )
         assert get_stream_value(logger) == expected_result
 
+    def test_no_changes(self, get_input, logger, config_file):
+        fake_stream = FakeInputStream([
+            '',
+            '',
+            '',
+            '',
+            '',
+        ])
+        get_input.side_effect = fake_stream
+
+        args = argparse.Namespace(target='foo')
+        config = {
+            'targets': {'foo': {
+                'local_folder': '/home/mark/documents',
+                's3_uri': 's3://buckets/mybackup',
+                'aws_access_key_id': '23123123123313',
+                'aws_secret_access_key': 'edwdadwadwadwdd',
+                'region_name': 'eu-west-1',
+            }}
+        }
+        cli.edit_command(args, config, logger)
+
+        with open(config_file, 'r') as fp:
+            config = json.load(fp)
+
+        expected_config = {
+            'targets': {'foo': {
+                'local_folder': '/home/mark/documents',
+                's3_uri': 's3://buckets/mybackup',
+                'aws_access_key_id': '23123123123313',
+                'aws_secret_access_key': 'edwdadwadwadwdd',
+                'region_name': 'eu-west-1',
+            }}
+        }
+        assert expected_config == config
+
+
     def test_correct_output(self, get_input, logger, config_file):
         fake_stream = FakeInputStream([
             '/home/user/Documents',
-            '',
-            '',
+            's3://buckets/mybackup222',
+            '9999999999',
             'bbbbbbbbbbbbbbbbbbbbbbbb',
             'eu-west-2',
         ])
@@ -110,8 +147,8 @@ class TestEditCommand(object):
         expected_config = {
             'targets': {'foo': {
                 'local_folder': '/home/user/Documents',
-                's3_uri': 's3://buckets/mybackup',
-                'aws_access_key_id': '23123123123313',
+                's3_uri': 's3://buckets/mybackup222',
+                'aws_access_key_id': '9999999999',
                 'aws_secret_access_key': 'bbbbbbbbbbbbbbbbbbbbbbbb',
                 'region_name': 'eu-west-2',
             }}
