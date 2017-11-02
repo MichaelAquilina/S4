a9e4a0b330245996a5e28c2b92c507982d1b5445
Michael Aquilina <michaelaquilina@gmail.com>
2017-11-02 10:33:45 +0000

    Move edit command to EditCommand class

--- s4/cli.py
+++ s4/cli.py
@@ -2,13 +2,13 @@
 
 import argparse
 import logging
-import os
 import sys
 
 from s4 import VERSION
 from s4 import utils
 from s4.commands.add_command import AddCommand
 from s4.commands.daemon_command import DaemonCommand
+from s4.commands.edit_command import EditCommand
 from s4.commands.ls_command import LsCommand
 from s4.commands.sync_command import SyncCommand
 from s4.commands.targets_command import TargetsCommand
@@ -149,48 +149,8 @@ def add_command(args, config, logger):
 
 
 def edit_command(args, config, logger):
-    if 'targets' not in config:
-        logger.info('You have not added any targets yet')
-        logger.info('Use the "add" command to do this')
-        return
-
-    if args.target not in config['targets']:
-        all_targets = sorted(list(config['targets'].keys()))
-        logger.info('"%s" is an unknown target', args.target)
-        logger.info('Choices are: %s', all_targets)
-        return
-
-    entry = config['targets'][args.target]
-
-    local_folder = entry.get('local_folder', '')
-    s3_uri = entry.get('s3_uri', '')
-    aws_access_key_id = entry.get('aws_access_key_id')
-    aws_secret_access_key = entry.get('aws_secret_access_key')
-    region_name = entry.get('region_name')
-
-    new_local_folder = utils.get_input('local folder [{}]: '.format(local_folder))
-    new_s3_uri = utils.get_input('s3 uri [{}]: '.format(s3_uri))
-    new_aws_access_key_id = utils.get_input('AWS Access Key ID [{}]: '.format(aws_access_key_id))
-
-    secret_key_prompt = 'AWS Secret Access Key [{}]: '.format(aws_secret_access_key)
-    new_aws_secret_access_key = utils.get_input(secret_key_prompt, secret=True)
-
-    new_region_name = utils.get_input('region name [{}]: '.format(region_name))
-
-    if new_local_folder:
-        entry['local_folder'] = os.path.expanduser(new_local_folder)
-    if new_s3_uri:
-        entry['s3_uri'] = new_s3_uri
-    if new_aws_access_key_id:
-        entry['aws_access_key_id'] = new_aws_access_key_id
-    if new_aws_secret_access_key:
-        entry['aws_secret_access_key'] = new_aws_secret_access_key
-    if new_region_name:
-        entry['region_name'] = new_region_name
-
-    config['targets'][args.target] = entry
-
-    utils.set_config(config)
+    command = EditCommand(args, config, logger)
+    command.run()
 
 
 def ls_command(args, config, logger):
