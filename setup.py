e3d7358e23ee63dc6144b55f2b5dcb8235b07e37
Michael Aquilina <michaelaquilina@gmail.com>
2018-05-20 19:53:38 +0100

    Several improvements to setup.py

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ with open('requirements.txt') as requirements_file:
     requirements = requirements_file.read().split('\n')
 
 setup(
-    name='s4',
+    name='S4',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     version=VERSION,
-    description='Sync your folders to s3 between multiple machines',
+    description='Fast and cheap synchronisation of files using Amazon S3',
     long_description=readme,
     install_requires=requirements,
     author='Michael Aquilina',
     license='GPLv3',
     author_email='michaelaquilina@gmail.com',
-    url='https://github.com/MichaelAquilina/s4',
+    url='https://github.com/MichaelAquilina/S4',
     python_requires=">=3.4",
     keywords='aws s3 backup sync',
     entry_points={
@@ -38,7 +38,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
