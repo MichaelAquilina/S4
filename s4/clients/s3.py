284aa12386faa1281c9633eb2e94085a87fc2f7c
ranaur <ranaur2008@ranaur.net>
2018-10-31 19:56:45 -0300

    ssl option was back! Removed it.

--- s4/clients/s3.py
+++ s4/clients/s3.py
@@ -23,13 +23,6 @@ logger = logging.getLogger(__name__)
 
 S3Uri = collections.namedtuple("S3Uri", ["bucket", "key"])
 
-ssl_cert = os.environ.get('REQUESTS_CA_BUNDLE', '')
-verifySSL = True
-if ssl_cert != '':
-    verifySSL = ssl_cert
-if ssl_cert == 'IGNORE':
-    verifySSL = False
-
 def get_s3_client(
     target, aws_access_key_id, aws_secret_access_key, endpoint_url, region_name
 ):
@@ -40,7 +33,6 @@ def get_s3_client(
         aws_secret_access_key=aws_secret_access_key,
         region_name=region_name,
         endpoint_url=endpoint_url,
-        verify=verifySSL
     )
     return S3SyncClient(s3_client, s3_uri.bucket, s3_uri.key)
 
